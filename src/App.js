0456b9b02969b317a45fb3c92112bd86393608a8
Rzayev221 <49637576+Rzayev221@users.noreply.github.com>
2023-02-06 19:27:23 +0100

    messages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 function App(){
 
-    return <h1>this message is coming from the App component</h1>
+    return (
+       <div className ="first class">
+    <h1>this message is coming from the App component</h1>
+    <p1>second message</p1>
+    
+        </div>
+    
+    )
 }
 
 export default App
