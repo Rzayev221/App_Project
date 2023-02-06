fe274d132d432cdf2e51eda3df3d132dc4e0cdd3
Rzayev221 <49637576+Rzayev221@users.noreply.github.com>
2023-02-06 20:21:00 +0100

    blabla

--- src/App.js
+++ src/App.js
@@ -3,13 +3,35 @@ function App(){
     const title = 'My Application'
     const body = "message"
 
-    return (
-       <div className ="first class">
 
-            <h1>{title}</h1>
+    const comments = [
+        {id:1 , text: "comment 1"},
+        {id: 2, text: "comment 2"}
+    ]
+
+    return (
+       <div className ='first class'>
+
+            <h1>{title.toUpperCase()}</h1>
             <p>{body}</p>
+
+        <div className="Comments">
+            <h3>Comments {comments.length}</h3>
+            <ul>
+                {comments.map((comment, index) =>
+                  <li>{comment.text}</li>
+                
+                )}
+              
+            </ul>
+
+
         </div>
-    
+
+
+     </div>
+
+
     )
 }
 
