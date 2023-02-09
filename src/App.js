6cd531b36afe661a395dd7dfd2b8bd244b24d4ea
Rzayev221 <49637576+Rzayev221@users.noreply.github.com>
2023-02-09 20:37:40 +0100

    falskda

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import Header from "./components/Header";
-
+import FeedbackItem from "./components/FeedbackItem";
 function App() {
 
  return(
     <>
-        <Header />
+        <Header/>
         <div className="container">
-        <h1> My App1</h1>
+        <FeedbackItem></FeedbackItem>
+
     </div>
     </>
     )
 }
-
 
 export default App
---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useState } from 'react'
+
+function FeedbackItem() {
+
+    const [rating, setRating] = useState(42)
+
+    
+    const [text, setText] = useState("flskdalsdaös")
+
+  
+
+
+
+  return (
+    <div className='card'>
+    <div className='num-display'>{rating}</div>
+    <div className='text-display'>{text}</div>
+    
+    </div>
+  )
+}
+
+export default FeedbackItem
